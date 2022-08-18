22b9886c336a94c91a2c596a03ce4d9a0fd5376f
erdemyusuf <111002966+erdemyusuf@users.noreply.github.com>
2022-08-18 23:31:30 +0300

    Update Gitignore.java

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,5 +1,6 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("ignore deneme 1");
+        // istedigimiz dosyalari gizlemeye yarar. aslinda private etmis oluyoruz
     }
 }
